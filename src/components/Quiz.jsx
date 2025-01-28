35c92027a8ae80f07e21ad9b28210fea8db2eb24
AleksandarBursac99 <aleksandarbursac150699@gmail.com>
2025-01-28 16:13:59 +0100

    Add quiz completion logic with summary screen and result image

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,25 +1,55 @@
 import { useState } from "react"
 import QUESTIONS from '../questions.js'
+import QuizCompleteImage from '../assets/quiz-complete.png';
 
 export default function Quiz(){
 
     // const[activeQuestionIndex, setActiveQuestionIndex] = useState(0);
     const[userAnswers, setUserAnswers] = useState([]);
+    
+
 
     const activeQuestionIndex = userAnswers.length;
+    // console.log(userAnswers);
+    
+    // console.log(activeQuestionIndex);
+
+    
+
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+    
 
     function handleSelectAnswer(selectedAnswer){
+        
         setUserAnswers((prevUserAnswers) => {
+            console.log(prevUserAnswers);
+            console.log(selectedAnswer);
+            
+            
             return[...prevUserAnswers, selectedAnswer];
         });
+        // console.log(selectedAnswer);
+        
+        
     }
+
+    if(quizIsComplete){
+        return(
+            <div id="summary">
+                <img src={QuizCompleteImage} alt="Trophy icon"/>
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort((a, b) => Math.random() - 0.5);
 
     return(
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                     <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer =>(
+                    {shuffledAnswers.map(answer =>(
                         <li key={answer} className="answer">
                         <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
